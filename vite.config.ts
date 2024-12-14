eae46531e5708cf896e379807c73749e3db15ec2
LukasBaran7 <lbaran95@gmail.com>
2024-12-14 13:15:58 +0100

    Refactor test setup and configuration files to include Vitest configuration (#11)

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { mergeConfig } from 'vite'
+import { defineConfig as defineVitestConfig } from 'vitest/config'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+const viteConfig = defineConfig({
+  plugins: [vue()]
+})
+
+const vitestConfig = defineVitestConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts']
+    exclude: ['e2e/*'],
+    include: ['./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
   }
 })
+
+export default mergeConfig(viteConfig, vitestConfig)
